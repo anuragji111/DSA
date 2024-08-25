ec8b633af1f219aedd9921019173c03c8de32772
Anurag <anurag@gmail.com>
2024-08-26 00:42:23 +0530

    first commit

---
+++ C++/Step 1-Learn the basics/Lec.1-Things.to.know.in.c++,java,python.or.any.other.language/1.User-Input-Output/program-1-The Generic Skeleton/Code.cpp
@@ -0,0 +1,8 @@
+// The generic skeleton of a C++ program consists of two main components: the library inclusion and the main function. After including the necessary libraries, you declare the main function using int main() { /* Your code here */ return 0; }. This serves as the entry point for your program.//
+
+#include<iostream>
+
+int main() {
+  //your code here
+  return 0;
+}
---
+++ C++/Step 1-Learn the basics/Lec.1-Things.to.know.in.c++,java,python.or.any.other.language/1.User-Input-Output/program-2/Code.cpp
@@ -0,0 +1,9 @@
+// Output with cout
+// To display output in C++, you'll commonly use the cout function from the iostream library. However, you need to specify that it belongs to the std (standard) namespace. For instance, std::cout << "Hey, Striver!"; will print "Hey, Striver!" to the console. You enclose the text you want to display within double quotation marks. //
+
+#include<iostream>
+
+int main() {
+    std::cout << "Hey, Light!";
+    return 0;
+}
